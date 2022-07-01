d4c2a555b817bbd021a8d082f857273c95c417c2
Jack Sweeny <web3jack@gmail.com>
2022-06-30 20:11:01 -0700

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import {MenuIcon} from '@heroicons/react/solid';
+import { MenuIcon } from '@heroicons/react/solid';
 import { useState } from "react";
 
 const Navbar = () => {
@@ -10,15 +10,15 @@ const Navbar = () => {
     return (
       <div className=" header">
         <div className="logo">Logo</div>
-        <nav  className= { menu ? "link-list-toggle absolute w-full md:top-0 md:relative md:flex md:w-auto md:h-full md:align-middle md:transition-none": "link-list absolute w-full md:top-0 md:relative md:flex md:w-auto md:h-full md:align-middle md:transition-none"}>
+        <nav  className= { menu ? "link-list-toggle absolute w-full md:top-0 md:relative md:flex md:w-auto md:h-full md:align-middle md:transition-none": "link-list absolute w-full md:top-0 md:relative md:flex md:w-auto md:h-full md:align-middle md:transition-none md:justify-center"}>
           <Link href={"/"}>
-            <a className="nav-link block text-lg py-3 text-center px-4 my-auto">Home</a>
+            <a className="nav-link block text-lg py-3 text-center px-4">Home</a>
           </Link>
           <Link href={"/contact"}>
-            <a className="nav-link block text-lg py-3 text-center  px-4 my-auto">Contact</a>
+            <a className="nav-link block text-lg py-3 text-center px-4">Contact</a>
           </Link>
           <Link href={"/projects"}>
-            <a className="nav-link block text-lg py-3 text-center px-4 my-auto z-0">Projects</a>
+            <a className="nav-link block text-lg py-3 text-center px-4 ">Projects</a>
           </Link>
         </nav>
         <MenuIcon onClick={toggleMenu} className=" h-8 w-8 cursor-pointer z-10 md:hidden" />
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <div className='hero bg-slate-200  rounded-md md:grid grid-cols-2 max-w-7xl mx-auto'>
             <div className="hero-text">
               <h2 className="text-5xl p-6"> Hero Text</h2>
-              <p className='mx-4 my-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, tempora facilis! Nulla architecto aperiam iste blanditiis veritatis hic aliquam nemo expedita autem! Sed, hic? Asperiores cupiditate nisi repudiandae inventore iste!</p>
+              <p className='mx-4 my-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nobis illo consequatur, voluptates veritatis molestiae eos perferendis incidunt saepe eligendi iusto, quos, natus corporis non veniam! Accusantium necessitatibus perferendis maxime alias? Dolores, pariatur? Soluta dolores at atque odio voluptatibus aperiam sint, autem excepturi, itaque expedita et labore rem accusamus delectus laudantium maiores in similique consequuntur eos. Quas enim repellat necessitatibus.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, tempora facilis! Nulla architecto aperiam iste blanditiis veritatis hic aliquam nemo expedita autem! Sed, hic? Asperiores cupiditate nisi repudiandae inventore iste!</p>
             </div>
 
             <div className="img-container mx-4 my-0">
@@ -16,6 +16,7 @@ export default function Home() {
                 width={900}
                 height={900}
                 objectFit={"contain"}
+                alt="hero image"
               />
             </div>
         </div>
@@ -38,9 +39,29 @@ export default function Home() {
 
         <h2 className='text-center text-4xl text-rose-400 my-6'>Projects</h2>
 
+        {/* Contact Section */}
+        <h2 className='text-center text-4xl text-rose-400 my-6'>Contact</h2>
+        <div className=" bg-blue-100  mx-auto  max-w-7xl rounded-md mb-80">
+            <h3 className='text-2xl text-center py-3'>Get in Touch!</h3>
+            <div className='max-w-3xl  md:mx-auto'>
+              <div className='ml-3'>
+                <label className='block text-sm ml-2 text-gray-700 font-bold'>Email: </label>
+                <input type="email" placeholder='email' className='px-4 py-2 outline-none rounded-md' />
+              </div>
+              
 
-
-
+              <div className="mx-3">
+                      <label className="block ml-2 text-sm pt-2 text-gray-700 font-bold">Comments:</label>
+                      <textarea  id="" rows="10" placeholder="What are you looking for?" className=" resize-none max-h-50 w-full max-w-3xl mx-auto my-2 px-2 py-3 outline-green-200 rounded-xl"></textarea>
+              </div>
+            </div>
+            
+        </div>
     </div>
+
+  
+    
+
+
   )
 }
