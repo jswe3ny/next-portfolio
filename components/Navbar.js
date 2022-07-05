8bf5d46ed098fea0065d499e323af6f813f8885a
Jack Sweeny <web3jack@gmail.com>
2022-07-05 13:25:03 -0700

    next commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,47 @@
 import Link from "next/link";
 import { MenuIcon } from '@heroicons/react/solid';
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
 
 const Navbar = () => {
 
   const [menu, setMenu] = useState(false);
     const toggleMenu = () => setMenu(!menu)
+
+    // let menuRef = useRef();
+    const ref = useRef();
+
+    
+
+    // const  useClickOutside = (ref, handler) =>{
+    //   useEffect(() => {
+    //    const listener = (event) => {
+    //     if(!ref.current || ref.current.contains(event.target)) {
+    //       return;
+    //     }
+
+    //     handler(event);
+    //    };
+    //    document.addEventListener("mousedown", listener);
+    //    document.addEventListener("touchstart", listener);
+
+    //    return () => {
+    //     document.removeEventListener("mousedown", listener);
+    //     document.removeEventListener("touchstart", listener);
+    //    }
+    //   },
+    //   [ref,handler]);
+      
+    // }
+
+    // useClickOutside(ref, () => setMenu(false));
+
+    
     // console.log(menu)
     return (
       <div className="header bg-primary">
         <div className="logo text-gray-50 text-lg">Logo</div>
-        <nav  className= { menu ? "link-list-toggle absolute w-full md:top-0 md:relative md:flex md:w-auto md:h-full md:align-middle md:transition-none": "link-list absolute w-full md:top-0 md:relative md:flex md:w-auto md:h-full md:align-middle md:transition-none md:justify-center"}>
+        <nav ref={ref} className= { menu ? "link-list-toggle absolute w-full md:top-0 md:relative md:flex md:w-auto md:h-full md:align-middle md:transition-none": "link-list absolute w-full md:top-0 md:relative md:flex md:w-auto md:h-full md:align-middle md:transition-none md:justify-center"}>
           <Link href={"/"}>
             <a className=" bg-primary nav-link block text-lg py-3 text-center px-4 text-gray-50 hover:bg-secondary hover:text-white">Home</a>
           </Link>
@@ -21,7 +52,7 @@ const Navbar = () => {
             <a className= "bg-primary nav-link block text-lg py-3 text-center px-4 text-gray-50 hover:bg-secondary hover:text-white">Projects</a>
           </Link>
         </nav>
-        <MenuIcon onClick={toggleMenu} className=" text-stone-50 h-8 w-8 cursor-pointer z-10 md:hidden" />
+        <MenuIcon id="toggleButton" onClick={toggleMenu} className=" text-stone-50 h-8 w-8 cursor-pointer z-10 md:hidden" />
 
       </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 
 import Image from 'next/image'
-import { useState } from 'react'
-import {useForm} from '@formspree/react';
-import styles from '../styles/Home.module.css';
 
 import{ FaReact, FaCss3Alt, FaHtml5 } from 'react-icons/fa';
 import {SiJavascript, SiFirebase} from'react-icons/si'
-import {IoLogoJavascript} from 'react-icons'
+
 import ContactForm from '../components/ContactForm';
 
 export default function Home() {
@@ -15,8 +12,8 @@ export default function Home() {
     <div className="mx-auto my-3 bg-gray-50">
         <div className='hero bg-gray-200 py-1  rounded-2xl md:grid grid-cols-2 max-w-7xl mx-auto md:py-0 '>
             <div className="hero-text mb-0">
-              <h2 className="text-5xl px-6 py-4 text-primary font-semibold"> Hero Text</h2>
-              <p className='mx-4 mt-0 pl-4 text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nobis illo consequatur, voluptates veritatis molestiae eos perferendis incidunt saepe eligendi iusto, quos, natus corporis non veniam! Accusantium necessitatibus perferendis maxime alias? Dolores, pariatur? Soluta dolores at atque odio voluptatibus aperiam sint, autem excepturi, itaque expedita et labore rem accusamus delectus laudantium maiores in similique consequuntur eos. Quas enim repellat necessitatibus.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, tempora facilis! Nulla architecto aperiam iste blanditiis veritatis hic aliquam nemo expedita autem! Sed, hic? Asperiores cupiditate nisi repudiandae inventore iste!</p>
+              <h2 className="text-5xl px-6 py-4 text-primary font-semibold">Jack Sweeny</h2>
+              <p className='mx-4 mt-0 pl-4 text-gray-800'>I am a university student who is also a self taught web developer. I am passionate about writing software, traveling, learning new thing Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione eaque debitis molestias id quam recusandae perspiciatis numquam ducimus vero, facilis, aperiam animi dicta commodi quod a unde! Aut, illo nemo!</p>
             </div>
 
             <div className="flex bg-black max-h-min mx-3 my-4 rounded-2xl md:grid md:mx-0 md:my-0 md:align-baseline md:object-cover">
@@ -32,28 +29,28 @@ export default function Home() {
         
         <h2 className='text-center text-4xl text-rose-400 my-6'>Tech Stack</h2>
         <div className="max-w-6xl flex-col items-center md:grid md:grid-cols-3 md:justify-items-center mx-auto gap-x-4 my-4">
-          <div className="card bg-gray-50 mx-auto w-80 my-4 border-2  rounded-md md:w-auto md:mx-4 md:my-0 h-96 lg:w-72">
+          <div className="card bg-gray-50 mx-auto w-80 my-4 border-2 rounded-2xl md:w-auto md:mx-4 md:my-0 h-96 lg:w-72">
             <h3 className='text-3xl text-primary my-3 font-semibold text-center'>Javascript</h3>
             <div className='flex justify-center'>
               {/* <FaHtml5 className='h-10 w-6 text-gray-900 mx-1' />
               <FaCss3Alt className='h-10 w-6 text-gray-900 ' /> */}
               <SiJavascript className='h-10 w-10 text-gray-900 mx-1' />
             </div>
-            <p className='mx-4 my-10 p-3 bg-blue-50'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero voluptates magni voluptatem excepturi saepe. Veritatis magnam ab deserunt dolores quae?</p>
+            <p className='mx-4 my-10 p-3'>Javascript data structures, ES6 as well as knowledge of object oriented programming</p>
           </div>
-          <div className="card bg-gray-50 mx-auto w-80 my-4  border-2 md:w-auto md:mx-4 md:my-0 h-96 lg:w-72">
+          <div className="card rounded-2xl bg-gray-50 mx-auto w-80 my-4  border-red-500 md:w-auto md:mx-4 md:my-0 h-96 lg:w-72">
             <h3 className='text-3xl text-primary my-3 font-semibold text-center'>React JS</h3>
             <div>
               <FaReact className='h-10 w-10 text-gray-900 mx-auto' />
             </div>
-            <p className='mx-4 my-10 p-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero voluptates magni voluptatem excepturi saepe. Veritatis magnam ab deserunt dolores quae?</p>
+            <p className='mx-4 my-10 p-3'>Use of React Hooks, APIs Server Side Rendering and Static Site Genteration with Next Js</p>
           </div>
-          <div className="card mx-auto w-80 my-4  border-2 md:w-auto md:mx-4 md:my-0 h-96 lg:w-72">
+          <div className="card rounded-2xl mx-auto w-80 my-4  border-2 md:w-auto md:mx-4 md:my-0 h-96 lg:w-72">
             <h3 className='text-3xl text-primary my-3 font-semibold text-center'>Firebase</h3>
             <div>
               <SiFirebase className='h-10 w-10 text-gray-900 mx-auto' />
             </div>
-            <p className='mx-4 my-10 p-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero voluptates magni voluptatem excepturi saepe. Veritatis magnam ab deserunt dolores quae?</p>
+            <p className='mx-4 my-10 p-3'>Experience with Firebase Products Such as Firestore, Realtime Database, and Firebase Authentication</p>
           </div>
         </div>
 
